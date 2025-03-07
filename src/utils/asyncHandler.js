c81990f12e663e43c0185ae8693b4da9fa3a7afe
Shreyanshs0ni <shreyanshsoni07@gmail.com>
2025-03-07 21:54:03 +0530

    connect to db, made util files, added app.js

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (requestHandler) => {
+  (res, req, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(error));
+  };
+};
+
+export { asyncHandler  };
+
+// const asyncHandler = (fn) => async (res, req, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
